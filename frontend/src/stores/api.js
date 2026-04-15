de19624c0b290ab0a4639c886833eb716a077652
Tiago Santos <tiago.m.santos.201323@gmail.com>
2026-04-15 17:45:29 +0100

    Fix

--- frontend/src/stores/api.js
+++ frontend/src/stores/api.js
@@ -4,7 +4,7 @@ import { inject } from 'vue'
 import {destroyEcho} from "@/websocket/echo.js";
 
 export const useAPIStore = defineStore('api', () => {
-    const API_BASE_URL = inject('apiBaseURL')
+    const API_BASE_URL = import.meta.env.VITE_BASE_URL + '/api';
 
     //set Barear
     const setBearerToken = (token) => {
