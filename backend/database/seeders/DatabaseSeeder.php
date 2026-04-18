a5dea9cf6dc6209eb7f7935f122ed1fffc8003ad
Tiago Santos <tiago.m.santos.201323@gmail.com>
2026-04-18 14:00:20 +0100

    fix: ultimo fix antes de entrega

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        if (User::all()->count()>0) {
+            return;
+        }
+
         //criar 4 admins
         User::factory()->count(2)->create([
             'role' => 'admin',
--- frontend/src/websocket/echo.js
+++ frontend/src/websocket/echo.js
@@ -25,7 +25,7 @@ export function getEcho() {
             },
         });
     }
-
+    console.log(echoInstance)
     return echoInstance;
 }
 
